dfe532d1bd53e323c6786f664330be328133207d
Maulik-009 <149244852+Maulik-009@users.noreply.github.com>
2024-01-02 11:31:27 +0530

    Basically created the whole website

---
+++ index1.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/logo.jpeg" >
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Electro World</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <div class="logotext">
+                    <a href="index1.html"><h1>ELECTRO WORLD</h1></a>
+                </div>
+                <div class="nav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">About Us</a></li>
+                    </ul>
+                </div>
+                <div class="search">
+                    <input type="search" name="Search" id="Search" placeholder="Search Your Product Here">
+                </div>
+                <div class="sign">
+                    <a href="Login-reg/index.php"><i class="fa-solid fa-circle-user" style="color: #ffffff;"></i>Sign in</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="options">
+            <div class="mobiles">
+               <a href="categories/category-page.html"><img src="images/mobiles.webp" alt="Mobiles"></a> 
+            </div>
+            <div class="television">
+                <a href=""><img src="images/television.webp" alt="Television"></a>
+            </div>
+            <div class="laptop">
+                <a href=""><img src="images/laptops.webp" alt="Laptops"></a>
+            </div>
+            <div class="he">
+                <a href=""><img src="images/headphone.webp" alt="Headphones & Earphones"></a>
+            </div>
+            <div class="tablet">
+                <a href=""><img src="images/tablet.webp" alt="Tablet"></a>
+            </div>
+            <div class="gaming">
+                <a href=""><img src="images/gaming.webp" alt="Gaming"></a>
+            </div>
+            <div class="wear">
+                <a href=""><img src="images/wearables.webp" alt="Wearables"></a>
+            </div>
+        </div>
+      
+    </main>
+</body>
+</html>
